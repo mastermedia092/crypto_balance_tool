22a502d267fd81297739ef73b654ab044d1381e4
mastermedia092 <mastermedia092@gmail.com>
2024-01-07 21:10:27 +0100

    add logic for native

--- chainbase.py
+++ chainbase.py
@@ -1,10 +1,13 @@
 import asyncio
 from loguru import logger
+from web3 import Web3
 
 from config import CHAINBASE_API_KEY, URLS
+from converters import convertHexToBalance
 from excel_exporter import ExcelExporter
 from network import Network
 from wallet import Wallet
+import os
 
 
 class Chainbase:
@@ -27,10 +30,19 @@ class Chainbase:
             ) as resp:
                 if resp.status == 200:
                     resp_json = await resp.json(content_type=None)
-                    if(resp_json["data"] == None):
+                    if resp_json["data"] is None:
                         logger.warning(f"no data for {address} on {Network.networks.get(chain_id)}")
                         return
-                    
+
+                    if isinstance(resp_json["data"], str):
+                        balance = convertHexToBalance(resp_json["data"])
+                        
+                        # Save result to txt file
+                        result = f"{address} - {chain_id} - {balance}"
+                        with open("balances.txt", "a") as file:
+                            file.write(result + "\n")
+                            return
+
                     wallet = Wallet(address, chain_id)
                     wallet.add_asset(resp_json["data"])
                     excel_exporter = ExcelExporter("balances.xlsx")
